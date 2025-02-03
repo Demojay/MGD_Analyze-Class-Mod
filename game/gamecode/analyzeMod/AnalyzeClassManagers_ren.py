dcbeed3a178d599393da05d63a836833211bf58e
Demojay <demojay45@gmail.com>
2025-02-03 17:35:44 +0000

    Added "NoAutoAnalyze" PerkType, which prevents a monster from being auto-analyzed if needed Added ability to add and remove class perk via the Goddess Statue

--- game/gamecode/analyzeMod/AnalyzeClassManagers_ren.py
+++ game/gamecode/analyzeMod/AnalyzeClassManagers_ren.py
@@ -1,9 +1,8 @@
-from renpy.store import Player, Monster, NoClassManager, classGenerators   # type: ignore
-from renpy import narrator
+from renpy.store import Player, Monster, Perk, NoClassManager, classGenerators, modPerkTypes   # type: ignore
 """renpy
 init 1 python:
 """
-from typing import Literal, Union
+from typing import Union
 
 class AnalyzeClassManager(NoClassManager):
 
@@ -14,6 +13,12 @@ class AnalyzeClassManager(NoClassManager):
     """
     CL_NAME = "Analyze"
 
+    global modPerkTypes
+    modPerkTypes.append("NoAutoAnalyze")
+
+    @staticmethod
+    def resetViewedMonsters():
+        AnalyzeClassManager.MONSTERS = []
 
 class AnalyzeOwnerClassManager(AnalyzeClassManager):
     CL_TYPE = "Owner"
@@ -21,15 +26,51 @@ class AnalyzeOwnerClassManager(AnalyzeClassManager):
 class AnalyzeTargetClassManager(AnalyzeClassManager):
     CL_TYPE = "Target"
 
+    
+    def _canBeAutoAnalysed(char: Monster) -> bool:
+        """
+        Internal function to see whether the given Monster has the "NoAutoAnalyze" perkType in their perks, meaning their status won't be automatically revealed
+        :param char: Holder of this instance
+        :type char: Monster
+        :return True if the monster has no perk with the "NoAutoAnalyze" perkType
+        :rtype bool
+        """
+        foundNoAutoAnalyze = False
+
+        for perk in char.perks:
+            if foundNoAutoAnalyze:
+                break
+
+            for perkType in perk.PerkType:
+                if perkType == "NoAutoAnalyze":
+                    foundNoAutoAnalyze = True
+                    break
+        
+        return not foundNoAutoAnalyze
+
     def __init__(self, char = None):
         super().__init__(char)
 
-        if char and type(char) is Monster and char.IDname in self.MONSTERS:
-            char.encyclopedia = "_Analyze"
+        if char and type(char) is Monster and char.IDname in AnalyzeClassManager.MONSTERS:
+            if self._canBeAutoAnalysed(char):
+                char.encyclopedia = "_Analyze"
     
     def analyzeEndChange(self, currTtip: str, char: 'Monster') -> str:
-        self.MONSTERS.append(char.IDname)
+        if char.IDname not in AnalyzeClassManager.MONSTERS:
+            AnalyzeClassManager.MONSTERS.append(char.IDname)
         return super().analyzeEndChange(currTtip, char)
+    
+    def applyModPerkEffects(self, owner: Union['Player', 'Monster'], perk: 'Perk', toAdd: bool) -> None:
+
+        """
+        Makes the monster instance who has been given this perkType not analyzed. For use with monsters
+        that spawn similar looking minions with them. Does not remove them from the internal viewed list. 
+        """
+        if type(owner) is Monster:
+            for perkType in perk.PerkType:
+                if perkType == "NoAutoAnalyze":
+                    owner.encyclopedia = ""
+                    break
 
 classGenerators["Analyze"]= { 
     "Owner": AnalyzeOwnerClassManager,
